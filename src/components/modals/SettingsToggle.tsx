9128345ed860196bc17db47383a8baa2041f80d6
Dimitar <dimitarduino@gmail.com>
2022-03-24 20:55:58 +0100

    style improvements

--- src/components/modals/SettingsToggle.tsx
+++ src/components/modals/SettingsToggle.tsx
@@ -14,7 +14,7 @@ export const SettingsToggle = ({
   description,
 }: Props) => {
   const toggleHolder = classnames(
-    'w-14 h-8 flex shrink-0 items-center bg-gray-300 rounded-full p-1 duration-300 ease-in-out cursor-pointer',
+    'w-16 h-8 flex shrink-0 items-center bg-gray-300 rounded-full p-1 duration-300 ease-in-out cursor-pointer',
     {
       'bg-green-400': flag,
     }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -20,17 +20,17 @@ export const Navbar = ({
     <div className="navbar">
       <div className="navbar-content px-5">
         <InformationCircleIcon
-          className="h-6 w-6 mr-2 cursor-pointer dark:stroke-white"
+          className="h-6 w-6 mr-2 cursor-pointer"
           onClick={() => setIsInfoModalOpen(true)}
         />
         <p className="text-xl ml-2.5 font-bold dark:text-white">{GAME_TITLE}</p>
         <div className="right-icons">
           <ChartBarIcon
-            className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
+            className="h-6 w-6 mr-3 cursor-pointer"
             onClick={() => setIsStatsModalOpen(true)}
           />
           <CogIcon
-            className="h-6 w-6 cursor-pointer dark:stroke-white"
+            className="h-6 w-6 cursor-pointer"
             onClick={() => setIsSettingsModalOpen(true)}
           />
         </div>
